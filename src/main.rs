01cee6c49e2099aa8a861b1567e03cca05dddf7d
Tomohiro Nakagawa <t-nakagw@sra.co.jp>
2022-01-27 09:03:20 +0900

    update to rust 1.53

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 extern crate calamine;
 extern crate regex;
 #[macro_use]
-extern crate lazy_static;
+extern crate once_cell;
 
 use std::error::Error;
 use std::env;
 use std::path::Path;
-use calamine::{Sheets, Range, Rows, DataType};
+use calamine::{Reader, Range, Rows, open_workbook_auto, DataType};
 use regex::Regex;
+use once_cell::sync::Lazy;
 
-lazy_static! {
-    static ref REGEX_N: Regex = Regex::new(r"\n").unwrap();
-    static ref REGEX_R: Regex = Regex::new(r"\r").unwrap();
-    static ref REGEX_T: Regex = Regex::new(r"\t").unwrap();
-    static ref REGEX_BS: Regex = Regex::new(r"\\").unwrap();
-}
+static REGEX_N:  Lazy<Regex> = Lazy::new(|| {Regex::new(r"\n").unwrap()});
+static REGEX_R:  Lazy<Regex> = Lazy::new(|| {Regex::new(r"\r").unwrap()});
+static REGEX_T:  Lazy<Regex> = Lazy::new(|| {Regex::new(r"\t").unwrap()});
+static REGEX_BS: Lazy<Regex> = Lazy::new(|| {Regex::new(r"\\").unwrap()});
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -22,39 +21,41 @@ fn main() {
         panic!("Usage: spreadsheet_textconv file.xlsx");
     }
     let file_path: &Path = Path::new(args.get(1).unwrap());
-    let mut workbook: Sheets = match Sheets::open(file_path) {
+    let mut workbook = match open_workbook_auto(file_path) {
         Err(why) => {
             let display = file_path.display();
             panic!("couldn't open {}: {}", display, Error::description(&why));
         }
         Ok(file) => file,
     };
-    let sheet_names: Vec<String> = workbook.sheet_names().unwrap();
+    let sheet_names = workbook.sheet_names().to_owned();
     for name in sheet_names {
-        let range: Range<DataType> = workbook.worksheet_range(&name).unwrap();
-        let rows: Rows<DataType> = range.rows();
-        let mut output: Vec<String> = Vec::new();
+		if let Some(Ok(range)) = workbook.worksheet_range(&name) {
+			let rows: Rows<DataType> = range.rows();
+			let mut output: Vec<String> = Vec::new();
 
-        for row in rows {
-            let sheet_name: String = "[".to_string() + &name.to_string() + "]\t";
-            output.push(sheet_name);
-            for cell in row.iter() {
-                let value = match *cell {
-                    DataType::Empty => "".to_string(),
-                    DataType::String(ref string) => string.to_string(),
-                    DataType::Float(ref float) => float.to_string(),
-                    DataType::Int(ref int) => int.to_string(),
-                    DataType::Bool(ref boolean) => boolean.to_string(),
-                    DataType::Error(ref err) => err.to_string(),
-                };
-                let replaced_value: String = replace_special_chars(value);
-                output.push(replaced_value);
-                output.push("\t".to_string());
-            }
-            output.push("\r\n".to_string());
-        }
-        let out = output.as_mut_slice().concat();
-        print!("{}", out);
+			for row in rows {
+				let sheet_name: String = "[".to_string() + &name.to_string() + "]\t";
+				output.push(sheet_name);
+				for cell in row.iter() {
+					let value = match *cell {
+						DataType::Empty => "".to_string(),
+						DataType::String(ref string) => string.to_string(),
+						DataType::Float(ref float) => float.to_string(),
+						DataType::Int(ref int) => int.to_string(),
+						DataType::Bool(ref boolean) => boolean.to_string(),
+						DataType::DateTime(ref float) => float.to_string(),
+						DataType::Error(ref err) => err.to_string(),
+					};
+					let replaced_value: String = replace_special_chars(value);
+					output.push(replaced_value);
+					output.push("\t".to_string());
+				}
+				output.push("\r\n".to_string());
+			}
+			let out = output.as_mut_slice().concat();
+			print!("{}", out);
+		}
     }
 }
 
